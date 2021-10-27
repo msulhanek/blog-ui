949debe2eaf4982a3f9c023cf2b4536327010c1c
msulhanek <m.sulhane@gmail.com>
2021-10-27 15:56:37 +0200

    2 - upgrade 11

--- src/app/comment-add/comment-add.component.spec.ts
+++ src/app/comment-add/comment-add.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CommentAddComponent } from './comment-add.component';
 
@@ -6,7 +6,7 @@ describe('CommentAddComponent', () => {
   let component: CommentAddComponent;
   let fixture: ComponentFixture<CommentAddComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CommentAddComponent ]
     })
--- src/app/comment-get/comment-get.component.spec.ts
+++ src/app/comment-get/comment-get.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CommentGetComponent } from './comment-get.component';
 
@@ -6,7 +6,7 @@ describe('CommentGetComponent', () => {
   let component: CommentGetComponent;
   let fixture: ComponentFixture<CommentGetComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CommentGetComponent ]
     })
